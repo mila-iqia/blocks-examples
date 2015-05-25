d18cd03f7acc701c99a92e527ff402249f3bec96
serdyuk <serdyuk.dmitriy@gmail.com>
2015-05-25 15:28:56 -0400

    Used custom pickler

--- tests/test_sqrt.py
+++ tests/test_sqrt.py
@@ -6,7 +6,7 @@ from sqrt import main
 
 
 def test_sqrt():
-    save_path = tempfile.mkdtemp()
+    save_path = tempfile.mktemp()
     main(save_path, 7)
     main_loop = main(save_path, 14, continue_=True)
     assert main_loop.log[7][SAVED_TO] == save_path

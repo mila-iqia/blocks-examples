9cc8dbe2425fc50fe5f685e54cc7ec57084bcd92
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-05-25 03:58:21 -0400

    machine translation initial commit

---
+++ machine_translation/sampling.py
@@ -0,0 +1,344 @@
+import logging
+import numpy
+import operator
+import os
+import re
+import signal
+import time
+
+from blocks.extensions import SimpleExtension
+from blocks.filter import VariableFilter
+from blocks.roles import OUTPUT
+from blocks.search import BeamSearch
+
+from collections import OrderedDict
+from picklable_itertools.extras import equizip
+from subprocess import Popen, PIPE
+
+from theano import function, tensor
+
+logger = logging.getLogger(__name__)
+
+
+class BeamSearchWMT15(BeamSearch):
+    """Extend BeamSearch for https://github.com/kyunghyuncho/NMT/issues/19"""
+
+    def __init__(self, prev_word, beam_size, samples):
+        super(BeamSearchWMT15, self).__init__(beam_size, samples)
+        self.prev_word = prev_word
+
+    def _compile_logprobs_computer(self):
+        """Overrides BeamSearch._compile_logprobs_computer."""
+        probs = VariableFilter(
+            applications=[self.generator.readout.emitter.probs],
+            roles=[OUTPUT])(self.inner_cg)[0]
+        logprobs = -tensor.log(probs)
+        self.logprobs_computer = function(
+            self.contexts + self.input_states + [self.prev_word], logprobs,
+            on_unused_input='ignore')
+
+    def compute_logprobs(self, contexts, states, outputs):
+        """Overrides BeamSearch.compute_logprobs."""
+        input_states = [states[name] for name in self.input_state_names]
+        return self.logprobs_computer(*(list(contexts.values()) +
+                                      input_states + [outputs.reshape(-1, 1)]))
+
+    def compute_next_states(self, contexts, states, outputs):
+        """Overrides BeamSearch.compute_next_states."""
+        input_states = [states[name] for name in self.input_state_names]
+        next_values = self.next_state_computer(*(list(contexts.values()) +
+                                                 input_states + [outputs]))
+        return OrderedDict(equizip(self.state_names, next_values))
+
+
+class SamplingBase(object):
+    """Utility class for BleuValidator and Sampler."""
+
+    def _get_attr_rec(self, obj, attr):
+        return self._get_attr_rec(getattr(obj, attr), attr) \
+            if hasattr(obj, attr) else obj
+
+    def _get_true_length(self, seq, vocab):
+        try:
+            return seq.tolist().index(vocab['</S>']) + 1
+        except ValueError:
+            return len(seq)
+
+    def _oov_to_unk(self, seq):
+        return [x if x < self.config['src_vocab_size'] else self.unk_idx
+                for x in seq]
+
+    def _parse_input(self, line):
+        seqin = line.split()
+        seqlen = len(seqin)
+        seq = numpy.zeros(seqlen+1, dtype='int64')
+        for idx, sx in enumerate(seqin):
+            seq[idx] = self.vocab.get(sx, self.unk_idx)
+            if seq[idx] >= self.config['src_vocab_size']:
+                seq[idx] = self.unk_idx
+        seq[-1] = self.eos_idx
+        return seq
+
+    def _idx_to_word(self, seq, ivocab):
+        return " ".join([ivocab.get(idx, "<UNK>") for idx in seq])
+
+
+class Sampler(SimpleExtension, SamplingBase):
+    """Random Sampling from model."""
+
+    def __init__(self, model, data_stream, config,
+                 src_vocab=None, trg_vocab=None, src_ivocab=None,
+                 trg_ivocab=None, **kwargs):
+        super(Sampler, self).__init__(**kwargs)
+        self.model = model
+        self.config = config
+        self.data_stream = data_stream
+        self.src_vocab = src_vocab
+        self.trg_vocab = trg_vocab
+        self.src_ivocab = src_ivocab
+        self.trg_ivocab = trg_ivocab
+        self.is_synced = False
+        self.sampling_fn = model.get_theano_function()
+
+    def do(self, which_callback, *args):
+
+        # Get current model parameters
+        if not self.is_synced:
+            self.model.params = self.main_loop.model.params
+            self.is_synced = True
+
+        # Get dictionaries, this may not be the practical way
+        sources = self._get_attr_rec(self.main_loop, 'data_stream')
+
+        # Load vocabularies and invert if necessary
+        # WARNING: Source and target indices from data stream
+        #  can be different
+        if not self.src_vocab:
+            self.src_vocab = sources.data_streams[0].dataset.dictionary
+        if not self.trg_vocab:
+            self.trg_vocab = sources.data_streams[1].dataset.dictionary
+        if not self.src_ivocab:
+            self.src_ivocab = {v: k for k, v in self.src_vocab.items()}
+        if not self.trg_ivocab:
+            self.trg_ivocab = {v: k for k, v in self.trg_vocab.items()}
+
+        # Randomly select source samples from the current batch
+        # WARNING: Source and target indices from data stream
+        #  can be different
+        batch = args[0]
+
+        # TODO: this is problematic for boundary conditions, eg. last batch
+        sample_idx = numpy.random.choice(
+            self.config['batch_size'], self.config['hook_samples'],
+            replace=False)
+        src_batch = batch[self.main_loop.data_stream.mask_sources[0]]
+        trg_batch = batch[self.main_loop.data_stream.mask_sources[1]]
+
+        input_ = src_batch[sample_idx, :]
+        target_ = trg_batch[sample_idx, :]
+
+        # Sample
+        _1, outputs, _2, _3, costs = (self.sampling_fn(input_))
+        outputs = outputs.T
+        costs = list(costs.T)
+
+        print ""
+        for i in range(len(outputs)):
+            input_length = self._get_true_length(input_[i], self.src_vocab)
+            target_length = self._get_true_length(target_[i], self.trg_vocab)
+            sample_length = self._get_true_length(outputs[i], self.trg_vocab)
+
+            print "Input : ", self._idx_to_word(input_[i][:input_length],
+                                                self.src_ivocab)
+            print "Target: ", self._idx_to_word(target_[i][:target_length],
+                                                self.trg_ivocab)
+            print "Sample: ", self._idx_to_word(outputs[i][:sample_length],
+                                                self.trg_ivocab)
+            print "Sample cost: ", costs[i][:sample_length].sum()
+            print ""
+
+
+class BleuValidator(SimpleExtension, SamplingBase):
+    """Implements early stopping based on BLEU score."""
+
+    def __init__(self, source_sentence, samples, model, data_stream,
+                 config, n_best=1, track_n_models=1, trg_ivocab=None,
+                 **kwargs):
+        super(BleuValidator, self).__init__(**kwargs)
+        self.source_sentence = source_sentence
+        self.samples = samples
+        self.model = model
+        self.data_stream = data_stream
+        self.config = config
+        self.n_best = n_best
+        self.track_n_models = track_n_models
+        self.verbose = config.get('val_set_out', None)
+
+        # Helpers
+        self.vocab = data_stream.dataset.dictionary
+        self.trg_ivocab = trg_ivocab
+        self.unk_sym = data_stream.dataset.unk_token
+        self.eos_sym = data_stream.dataset.eos_token
+        self.unk_idx = self.vocab[self.unk_sym]
+        self.eos_idx = self.vocab[self.eos_sym]
+        self.best_models = []
+        self.val_bleu_curve = []
+        self.beam_search = BeamSearchWMT15(
+            source_sentence, beam_size=self.config['beam_size'],
+            samples=samples)
+        self.multibleu_cmd = ['perl', self.config['bleu_script'],
+                              self.config['val_set_grndtruth'], '<']
+
+        # Create saving directory if it does not exist
+        if not os.path.exists(self.config['saveto']):
+            os.makedirs(self.config['saveto'])
+
+        if self.config['reload']:
+            try:
+                bleu_score = numpy.load(os.path.join(self.config['saveto'],
+                                        'val_bleu_scores.npz'))
+                self.val_bleu_curve = bleu_score['bleu_scores'].tolist()
+
+                # Track n best previous bleu scores
+                for i, bleu in enumerate(
+                        sorted(self.val_bleu_curve, reverse=True)):
+                    if i < self.track_n_models:
+                        self.best_models.append(ModelInfo(bleu))
+                logger.info("BleuScores Reloaded")
+            except:
+                logger.info("BleuScores not Found")
+
+    def do(self, which_callback, *args):
+
+        # Track validation burn in
+        if self.main_loop.status['iterations_done'] <= \
+                self.config['val_burn_in']:
+            return
+
+        # Get current model parameters
+        self.model.set_param_values(
+            self.main_loop.model.get_param_values())
+
+        # Evaluate and save if necessary
+        self._save_model(self._evaluate_model())
+
+    def _evaluate_model(self):
+
+        logger.info("Started Validation: ")
+        val_start_time = time.time()
+        mb_subprocess = Popen(self.multibleu_cmd, stdin=PIPE, stdout=PIPE)
+        total_cost = 0.0
+
+        # Get target vocabulary
+        if not self.trg_ivocab:
+            sources = self._get_attr_rec(self.main_loop, 'data_stream')
+            trg_vocab = sources.data_streams[1].dataset.dictionary
+            self.trg_ivocab = {v: k for k, v in trg_vocab.items()}
+
+        if self.verbose:
+            ftrans = open(self.config['val_set_out'], 'w')
+
+        for i, line in enumerate(self.data_stream.get_epoch_iterator()):
+            """
+            Load the sentence, retrieve the sample, write to file
+            """
+
+            seq = self._oov_to_unk(line[0])
+            input_ = numpy.tile(seq, (self.config['beam_size'], 1))
+
+            # draw sample, checking to ensure we don't get an empty string back
+            trans, costs = \
+                self.beam_search.search(
+                    input_values={self.source_sentence: input_},
+                    max_length=3*len(seq), eol_symbol=self.eos_idx,
+                    ignore_first_eol=True)
+
+            nbest_idx = numpy.argsort(costs)[:self.n_best]
+            for j, best in enumerate(nbest_idx):
+                try:
+                    total_cost += costs[best]
+                    trans_out = trans[best]
+
+                    # convert idx to words
+                    trans_out = self._idx_to_word(trans_out, self.trg_ivocab)
+
+                except ValueError:
+                    print "Can NOT find a translation for line: {}".format(i+1)
+                    trans_out = '<UNK>'
+
+                if j == 0:
+                    # Write to subprocess and file if it exists
+                    print >> mb_subprocess.stdin, trans_out
+                    if self.verbose:
+                        print >> ftrans, trans_out
+
+            if i != 0 and i % 100 == 0:
+                print "Translated {} lines of validation set...".format(i)
+
+            mb_subprocess.stdin.flush()
+
+        print "Total cost of the validation: {}".format(total_cost)
+        self.data_stream.reset()
+        if self.verbose:
+            ftrans.close()
+
+        # send end of file, read output.
+        mb_subprocess.stdin.close()
+        stdout = mb_subprocess.stdout.readline()
+        print "output ", stdout
+        out_parse = re.match(r'BLEU = [-.0-9]+', stdout)
+        logger.info("Validation Took: {} minutes".format(
+            float(time.time() - val_start_time) / 60.))
+        assert out_parse is not None
+
+        # extract the score
+        bleu_score = float(out_parse.group()[6:])
+        self.val_bleu_curve.append(bleu_score)
+        print bleu_score
+        mb_subprocess.terminate()
+
+        return bleu_score
+
+    def _is_valid_to_save(self, bleu_score):
+        if not self.best_models or min(self.best_models,
+           key=operator.attrgetter('bleu_score')).bleu_score < bleu_score:
+            return True
+        return False
+
+    def _save_model(self, bleu_score):
+        if self._is_valid_to_save(bleu_score):
+            model = ModelInfo(bleu_score, self.config['saveto'])
+
+            # Manage n-best model list first
+            if len(self.best_models) >= self.track_n_models:
+                old_model = self.best_models[0]
+                if old_model.path and os.path.isfile(old_model.path):
+                    logger.info("Deleting old model %s" % old_model.path)
+                    os.remove(old_model.path)
+                self.best_models.remove(old_model)
+
+            self.best_models.append(model)
+            self.best_models.sort(key=operator.attrgetter('bleu_score'))
+
+            # Save the model here
+            s = signal.signal(signal.SIGINT, signal.SIG_IGN)
+            logger.info("Saving new model {}".format(model.path))
+            numpy.savez(model.path, **self.main_loop.model.get_param_values())
+            numpy.savez(
+                os.path.join(self.config['saveto'], 'val_bleu_scores.npz'),
+                bleu_scores=self.val_bleu_curve)
+            signal.signal(signal.SIGINT, s)
+
+
+class ModelInfo:
+    """Utility class to keep track of evaluated models."""
+
+    def __init__(self, bleu_score, path=None):
+        self.bleu_score = bleu_score
+        self.path = self._generate_path(path)
+
+    def _generate_path(self, path):
+        gen_path = os.path.join(
+            path, 'best_bleu_model_%d_BLEU%.2f.npz' %
+            (int(time.time()), self.bleu_score) if path else None)
+        return gen_path

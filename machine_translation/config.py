cd8e7ca2ba92cd2b7393cc80a9fb5daf4634224d
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-06-30 13:51:32 -0400

    sync with central and add some comments

--- machine_translation/config.py
+++ machine_translation/config.py
@@ -1,52 +1,114 @@
 def get_config_cs2en():
     config = {}
 
-    # Model related
+    # Model related -----------------------------------------------------------
+
+    # Sequences longer than this will be discarded
     config['seq_len'] = 50
-    config['enc_nhids'] = 1000
-    config['dec_nhids'] = 1000
-    config['enc_embed'] = 620
-    config['dec_embed'] = 620
+
+    # Number of hidden units in encoder/decoder GRU
+    config['enc_nhids'] = 100
+    config['dec_nhids'] = 100
+
+    # Dimension of the word embedding matrix in encoder/decoder
+    config['enc_embed'] = 62
+    config['dec_embed'] = 62
+
+    # Where to save model
     config['saveto'] = 'search_model_cs2en'
 
-    # Optimization related
+    # Optimization related ----------------------------------------------------
+
+    # Batch size
     config['batch_size'] = 80
+
+    # This many batches will be read ahead and sorted
     config['sort_k_batches'] = 12
+
+    # Optimization step rule
     config['step_rule'] = 'AdaDelta'
-    config['step_clipping'] = 10
+
+    # Gradient clipping threshold
+    config['step_clipping'] = 1
+
+    # Std of weight initialization
     config['weight_scale'] = 0.01
 
-    # Regularization related
+    # Regularization related --------------------------------------------------
+
+    # Weight noise flag for feed forward layers
     config['weight_noise_ff'] = False
+
+    # Weight noise flag for recurrent layers
     config['weight_noise_rec'] = False
+
+    # Dropout ratio, applied only after readout maxout
     config['dropout'] = 1.0
 
-    # Vocabulary/dataset related
-    datadir = '/data/lisatmp3/firatorh/nmt/wmt15/data/fi-en/processed/'
-    config['stream'] = 'cs-en'
-    config['src_vocab'] = datadir + 'vocab.cs.pkl'
-    config['trg_vocab'] = datadir + 'vocab.en.pkl'
+    # Vocabulary/dataset related ----------------------------------------------
+
+    # Root directory for dataset
+    datadir = '/data/lisatmp3/firatorh/nmt/wmt15/data/cs-en/'
+
+    # Module name of the stream that will be used
+    config['stream'] = 'stream_cs2en'
+
+    # Source and target vocabularies
+    config['src_vocab'] = datadir + 'all.tok.clean.shuf.cs-en.cs.vocab.pkl'
+    config['trg_vocab'] = datadir + 'all.tok.clean.shuf.cs-en.en.vocab.pkl'
+
+    # Source and target datasets
     config['src_data'] = datadir + 'all.tok.clean.shuf.cs-en.cs'
     config['trg_data'] = datadir + 'all.tok.clean.shuf.cs-en.en'
+
+    # Source and target vocabulary sizes
     config['src_vocab_size'] = 40000
     config['trg_vocab_size'] = 40000
+
+    # Index of unknown token
     config['unk_id'] = 1
 
-    # Early stopping based on bleu related
+    # Early stopping based on bleu related ------------------------------------
+
+    # Normalize cost according to sequence length after beam-search
     config['normalized_bleu'] = True
+
+    # Bleu script that will be used (moses multi-perl in this case)
     config['bleu_script'] = datadir + 'multi-bleu.perl'
-    config['val_set'] = datadir + '/newsdev2015.tok.seg.fi'
-    config['val_set_grndtruth'] = data + '/newsdev2015_1.tok.en'
-    config['val_set_out'] = config['saveto'] + '/adadelta_40k_out.txt'
+
+    # Validation set source file
+    config['val_set'] = datadir + 'newsdev2013.tok.cs'
+
+    # Validation set gold file
+    config['val_set_grndtruth'] = datadir + 'newsdev2013.tok.en'
+
+    # Print validation output to file
     config['output_val_set'] = True
+
+    # Validation output file
+    config['val_set_out'] = config['saveto'] + '/adadelta_40k_out.txt'
+
+    # Beam-size
     config['beam_size'] = 20
 
-    # Timing/monitoring related
+    # Timing/monitoring related -----------------------------------------------
+
+    # Reload model from files if exist
     config['reload'] = True
+
+    # Save model after this many updates
     config['save_freq'] = 50
+
+    # Show samples from model after this many updates
     config['sampling_freq'] = 1
-    config['bleu_val_freq'] = 2000
-    config['val_burn_in'] = 50000
+
+    # Show this many samples at each sampling
     config['hook_samples'] = 1
+
+    # Validate bleu after this many updates
+    config['bleu_val_freq'] = 2000
+
+    # Start bleu validation after this many updates
+    config['val_burn_in'] = 50000
 
     return config
---
+++ machine_translation/stream_cs2en.py
@@ -0,0 +1,91 @@
+# TODO: Remove parametrized ifilter dependency
+# TODO: Remove MappingWithArgs
+# TODO: Remove FilterWithArgs
+import cPickle
+
+from fuel.datasets import TextFile
+from fuel.schemes import ConstantScheme
+from fuel.streams import DataStream
+from fuel.transformers import (
+    Merge, Batch, Filter, Padding, SortMapping, Unpack, Mapping)
+
+from model import config
+
+
+def _length(sentence_pair):
+    """Assumes target is the last element in the tuple."""
+    return len(sentence_pair[-1])
+
+
+class _oov_to_unk(object):
+    """Maps out of vocabulary token index to unk token index."""
+    def __init__(self, src_vocab_size=30000, trg_vocab_size=30000,
+                 unk_id=1):
+        self.src_vocab_size = src_vocab_size
+        self.trg_vocab_size = trg_vocab_size
+        self.unk_id = unk_id
+
+    def __call__(self, sentence_pair):
+        return ([x if x < self.src_vocab_size else self.unk_id
+                 for x in sentence_pair[0]],
+                [x if x < self.trg_vocab_size else self.unk_id
+                 for x in sentence_pair[1]])
+
+
+class _too_long(object):
+    """Filters sequences longer than given sequence length."""
+    def __init__(self, seq_len=50):
+        self.seq_len = seq_len
+
+    def __call__(self, sentence_pair):
+        return all([len(sentence) <= self.seq_len
+                    for sentence in sentence_pair])
+
+# Get helpers
+cs_vocab = config['src_vocab']
+en_vocab = config['trg_vocab']
+cs_file = config['src_data']
+en_file = config['trg_data']
+
+# Get text files from both source and target
+cs_dataset = TextFile([cs_file], cPickle.load(open(cs_vocab)), None)
+en_dataset = TextFile([en_file], cPickle.load(open(en_vocab)), None)
+
+# Merge them to get a source, target pair
+stream = Merge([cs_dataset.get_example_stream(),
+                en_dataset.get_example_stream()],
+               ('source', 'target'))
+
+# Filter sequences that are too long
+stream = Filter(stream, predicate=_too_long,
+                predicate_args={'seq_len': config['seq_len']})
+
+# Replace out of vocabulary tokens with unk token
+stream = Mapping(stream,
+                 _oov_to_unk(src_vocab_size=config['src_vocab_size'],
+                             trg_vocab_size=config['trg_vocab_size'],
+                             unk_id=config['unk_id']))
+
+# Build a batched version of stream to read k batches ahead
+stream = Batch(stream,
+               iteration_scheme=ConstantScheme(
+                   config['batch_size']*config['sort_k_batches']))
+
+# Sort all samples in the read-ahead batch
+stream = Mapping(stream, SortMapping(_length))
+
+# Convert it into a stream again
+stream = Unpack(stream)
+
+# Construct batches from the stream with specified batch size
+stream = Batch(stream, iteration_scheme=ConstantScheme(config['batch_size']))
+
+# Pad sequences that are short
+masked_stream = Padding(stream)
+
+# Setup development set stream if necessary
+dev_stream = None
+if 'val_set' in config and config['val_set']:
+    dev_file = config['val_set']
+    dev_dataset = TextFile([dev_file], cPickle.load(open(cs_vocab)), None)
+    dev_stream = DataStream(dev_dataset)

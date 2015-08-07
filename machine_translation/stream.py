7697b2f6a08f435da70ae945b8b005134d6b910c
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-08-07 11:23:00 -0400

    remove config from stream

--- machine_translation/stream.py
+++ machine_translation/stream.py
@@ -94,28 +94,24 @@ class _too_long(object):
                     for sentence in sentence_pair])
 
 
-def get_tr_stream(config):
+def get_tr_stream(src_vocab, trg_vocab, src_data, trg_data,
+                  src_vocab_size=30000, trg_vocab_size=30000, unk_id=1,
+                  seq_len=50, batch_size=80, sort_k_batches=12, **kwargs):
     """Prepares the training data stream."""
 
-    # Get helpers
-    src_vocab_file = config['src_vocab']
-    trg_vocab_file = config['trg_vocab']
-    src_file = config['src_data']
-    trg_file = config['trg_data']
-
     # Load dictionaries and ensure special tokens exist
-    src_vocab = _ensure_special_tokens(cPickle.load(open(src_vocab_file)),
+    src_vocab = _ensure_special_tokens(cPickle.load(open(src_vocab)),
                                        bos_idx=0,
-                                       eos_idx=config['src_vocab_size'] - 1,
-                                       unk_idx=config['unk_id'])
-    trg_vocab = _ensure_special_tokens(cPickle.load(open(trg_vocab_file)),
+                                       eos_idx=src_vocab_size - 1,
+                                       unk_idx=unk_id)
+    trg_vocab = _ensure_special_tokens(cPickle.load(open(trg_vocab)),
                                        bos_idx=0,
-                                       eos_idx=config['trg_vocab_size'] - 1,
-                                       unk_idx=config['unk_id'])
+                                       eos_idx=trg_vocab_size - 1,
+                                       unk_idx=unk_id)
 
     # Get text files from both source and target
-    src_dataset = TextFile([src_file], src_vocab, None)
-    trg_dataset = TextFile([trg_file], trg_vocab, None)
+    src_dataset = TextFile([src_data], src_vocab, None)
+    trg_dataset = TextFile([trg_data], trg_vocab, None)
 
     # Merge them to get a source, target pair
     stream = Merge([src_dataset.get_example_stream(),
@@ -124,18 +120,18 @@ def get_tr_stream(config):
 
     # Filter sequences that are too long
     stream = Filter(stream,
-                    predicate=_too_long(seq_len=config['seq_len']))
+                    predicate=_too_long(seq_len=seq_len))
 
     # Replace out of vocabulary tokens with unk token
     stream = Mapping(stream,
-                     _oov_to_unk(src_vocab_size=config['src_vocab_size'],
-                                 trg_vocab_size=config['trg_vocab_size'],
-                                 unk_id=config['unk_id']))
+                     _oov_to_unk(src_vocab_size=src_vocab_size,
+                                 trg_vocab_size=trg_vocab_size,
+                                 unk_id=unk_id))
 
     # Build a batched version of stream to read k batches ahead
     stream = Batch(stream,
                    iteration_scheme=ConstantScheme(
-                       config['batch_size']*config['sort_k_batches']))
+                       batch_size*sort_k_batches))
 
     # Sort all samples in the read-ahead batch
     stream = Mapping(stream, SortMapping(_length))
@@ -145,23 +141,23 @@ def get_tr_stream(config):
 
     # Construct batches from the stream with specified batch size
     stream = Batch(
-        stream, iteration_scheme=ConstantScheme(config['batch_size']))
+        stream, iteration_scheme=ConstantScheme(batch_size))
 
     # Pad sequences that are short
     masked_stream = PaddingWithEOS(
-        stream, [config['src_vocab_size'] - 1, config['trg_vocab_size'] - 1])
+        stream, [src_vocab_size - 1, trg_vocab_size - 1])
 
     return masked_stream
 
 
-def get_dev_stream(config):
+def get_dev_stream(val_set=None, src_vocab=None, src_vocab_size=30000,
+                   unk_id=1, **kwargs):
     """Setup development set stream if necessary."""
     dev_stream = None
-    if 'val_set' in config and config['val_set']:
-        dev_file = config['val_set']
+    if val_set is not None and src_vocab is not None:
         src_vocab = _ensure_special_tokens(
-            cPickle.load(open(config['src_vocab'])), bos_idx=0,
-            eos_idx=config['src_vocab_size'] - 1, unk_idx=config['unk_id'])
-        dev_dataset = TextFile([dev_file], src_vocab, None)
+            cPickle.load(open(src_vocab)), bos_idx=0,
+            eos_idx=src_vocab_size - 1, unk_idx=unk_id)
+        dev_dataset = TextFile([val_set], src_vocab, None)
         dev_stream = DataStream(dev_dataset)
     return dev_stream

c5ebe23dbea835eb3e47a4f157d85c38b344d7fc
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-08-09 12:27:43 -0400

    python3 compatibility for print

--- machine_translation/checkpoint.py
+++ machine_translation/checkpoint.py
@@ -77,7 +77,7 @@ class CheckpointNMT(SimpleExtension, SaveLoadUtils):
     def dump(self, main_loop):
         if not os.path.exists(self.path_to_folder):
             os.mkdir(self.path_to_folder)
-        print ""
+        print("")
         logger.info(" Saving model")
         start = time.time()
         logger.info(" ...saving parameters")
